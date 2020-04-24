80225478ba5c9476a04338b6b21d64bbfce5cbeb
sarahebrooks12 <sarahebrooks12@gmail.com>
2020-04-24 08:50:46 -0400

    nuclear redo for java

--- src/journal.js
+++ src/journal.js
@@ -1,22 +1,55 @@
-// <!-- Single Journal Entry You defined form fields for the following bits of data to collect from a user of your application. Date of the journal entry. Concepts covered (which will be the title of the entry). The long-form contents of the journal entry. The mood of the journal entry. In your JavaScript file, define an object that will represent a journal entry in your code. /* Define the keys and value for a JavaScript object that represents a journal entry about what you learned today */ const journalEntry
-// = {}
-// Journal Entry Collection 
-// Eventually, you will have multiple journal entries, so you need to define an appropriately named variable that will have the value of an array.Once you define that variable and give it a
-// default value of a blank array,
-// use the push() method to add the journalEntry object you defined above to the array.More Journal Entries Take a few minutes and define two or three more JavaScript objects to represent journal entries
-// for some other things that you have learned about
-// so far at NSS.Add those new objects to your journal entries array. -->
-
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+    date
+    concept
+    entry
+    mood
+*/
 const journalEntry = {
-    date: "today",
-    typeText: "concept",
-    entry: "nonsense",
-    mood: "okay"
+  date: "04/15/2020",
+  concept: "JavaScript objects",
+  entry: "nothing really makes sense but Jordan and Tommy keep saying to trust the process. That isn't easy for me but I'm going to keep chugging.",
+  mood: "stressed"
+}
+const journalEntry2 = {
+  date: "04/17/2020",
+  concept: "Printing to the DOM",
+  entry: "I'm trying to get all of the separate things I know to combine in my head but it just isn't happening. I can feel that the click is close but it is so frustrating to not be excelling at this.",
+  mood: "stressed"
+}
+const journalEntry3 = {
+  date: "04/18/2020",
+  concept: "manipulating the DOM",
+  entry: "I definitely am having such severe imposter syndrome. I feel like an absolute failure and like I will never understand any of this. I have had trouble with the whole program, but I didn't expect to succeed overnight. These last two days have been so tough. Even Tommy said that when he went through this with the first cohort, he really struggled.",
+  mood: "stressed"
+}
+const completeEntry = []
+push(journalEntry)
+push(journalEntry2)
+push(journalEntry3)
+
+/*
+    Purpose: To create, and return, a string template that
+    represents a single journal entry object as HTML
+
+    Arguments: journalEntry (object)
+*/
+const makeJournalEntryComponent = (completeEntry) => {
+  return `
+  <section>${completeEntry}</section>
+  `
 }
 
-const journalArray = []
-// console.log(journalArray)
-journalArray.push(journalEntry)
-// console.log(journalArray)
+/*
+    Purpose: To render all journal entries to the DOM
 
-console.log(journalArray[0])
+    Arguments: entries (array of objects)
+*/
+const renderJournalEntries = (entries) => {
+  document.querySelector("#entryLog").innerHTML += completeEntry
+
+}
+
+// Invoke the render function
+renderJournalEntries(completeEntry)
